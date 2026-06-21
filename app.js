19b911ff1b53805183bf56eb2035fb4077c60a71
Amar Kumar Singh <144318430+amar18git@users.noreply.github.com>
2026-06-21 21:50:27 +0530

    Configure CORS to allow credentials and origin

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: true, credentials: true}));
 app.use(express.json());
 
 app.use(cookieParser());
