87258d6f44015160f02b7046288fbd87c87b754b
Amar Kumar Singh <144318430+amar18git@users.noreply.github.com>
2026-06-21 14:06:41 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -51,9 +51,15 @@ router.post("/login", async (req, res) => {
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
-    
+
+    res.cookie("token", token, {  
+      httpOnly: true,
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+      sameSite: "lax"
+    });
     res.json({ message: "Login successful", token });
-    res.cookie("token", token);
+
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
