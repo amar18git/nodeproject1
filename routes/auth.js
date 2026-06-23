7e036d00f68010d1ab45a1bf2fd707da675c484b
Amar Kumar Singh <144318430+amar18git@users.noreply.github.com>
2026-06-23 21:36:25 +0530

    Make secure cookie setting environment-dependent

--- routes/auth.js
+++ routes/auth.js
@@ -54,7 +54,7 @@ router.post("/login", async (req, res) => {
 
     res.cookie("token", token, {  
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
       maxAge: 24 * 60 * 60 * 1000,
       sameSite: "lax"
     });
