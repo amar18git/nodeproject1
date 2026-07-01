bab0e3275c51aa3490c5627ac6eb8b88ed926258
Amar Kumar Singh <144318430+amar18git@users.noreply.github.com>
2026-07-01 17:20:02 +0530

    Enhance user schema with additional fields
    Added new fields to user schema including username, mobile, address, dob, aadhaar, pan, memberSince, verificationStatus, and chariteanId.

--- models/User.js
+++ models/User.js
@@ -1,9 +1,61 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-});
+  name: {
+    type: String,
+    required: true
+  },
+
+  username: {
+    type: String,
+    unique: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  mobile: {
+    type: String
+  },
+
+  address: {
+    type: String
+  },
+
+  dob: {
+    type: Date
+  },
+
+  aadhaar: {
+    type: String
+  },
+
+  pan: {
+    type: String
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  memberSince: {
+    type: Number,
+    default: new Date().getFullYear()
+  },
+
+  verificationStatus: {
+    type: String,
+    default: "0/3 Verified"
+  },
+
+  chariteanId: {
+    type: String
+  }
+
+}, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
